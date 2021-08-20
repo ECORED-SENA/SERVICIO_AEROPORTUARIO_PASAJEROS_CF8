0dbedbf95fb4452690752a660379f2643acd781c
luisEfe80 <luismendez80@hotmail.com>
2021-08-20 01:17:01 -0400

    Adicion de contenido

--- src/config/global.js
+++ src/config/global.js
@@ -51,97 +51,14 @@ export default {
             titulo: 'Listas',
             hash: 'listas',
           },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.5',
-            titulo: 'Tablas',
-            hash: 'tablas',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.6',
-            titulo: 'Imagen',
-            hash: 'imagen',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.7',
-            titulo: 'Imagen Infográfica',
-            hash: 'imagen_infografica',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.8',
-            titulo: 'Video',
-            hash: 'video',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.9',
-            titulo: 'Bloques de texto destacado',
-            hash: 'texto_destacado',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.10',
-            titulo: 'Sliders',
-            hash: 'sliders',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.11',
-            titulo: 'Acordiones',
-            hash: 'acordiones',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.12',
-            titulo: 'Pestañas o Tabs',
-            hash: 'tabs',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.13',
-            titulo: 'Líneas de tiempo',
-            hash: 'lineas_tiempo',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.14',
-            titulo: 'Pasos',
-            hash: 'pasos',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.15',
-            titulo: 'Llamados a la acción',
-            hash: 'llamados_accion',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.16',
-            titulo: 'Tarjetas',
-            hash: 'tarjetas',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.17',
-            titulo: 'Tarjetas con número',
-            hash: 'tarjeta_numero',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.18',
-            titulo: 'Modal',
-            hash: 'modal',
-          },
         ],
       },
       {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Producción Audiovisual',
+        titulo:
+          'Passenger boarding process // <i>Proceso de embarque del pasajero</i>',
         desarrolloContenidos: true,
         subMenu: [
           {
@@ -156,86 +73,10 @@ export default {
             titulo: 'Video Motion',
             hash: 't_2_2',
           },
-          {
-            icono: 'far fa-file-alt',
-            numero: '2.3',
-            titulo: 'Motion + Presentador',
-            hash: 't_2_3',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '2.4',
-            titulo: 'Animación 2D',
-            hash: 't_2_4',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '2.5',
-            titulo: 'Video interactivo',
-            hash: 't_2_5',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '2.6',
-            titulo: 'Infografías',
-            hash: 't_2_6',
-          },
-        ],
-      },
-      {
-        nombreRuta: 'tema3',
-        icono: 'far fa-file-alt',
-        numero: '3',
-        titulo: 'Fundamentación Textil',
-        desarrolloContenidos: true,
-        subMenu: [
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.1',
-            titulo:
-              'Clasificación y conceptualización de las fibras naturales, químicas (artificiales y sintéticas).',
-            hash: 't_3_1',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.2',
-            titulo: 'Identificación fibras por combustión',
-            hash: 't_3_2',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.3',
-            titulo: 'Clasificación de los hilos',
-            hash: 't_3_3',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.4',
-            titulo: 'Clasificación de tejidos',
-            hash: 't_3_4',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.5',
-            titulo: 'Caracterización de los tejidos',
-            hash: 't_3_5',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.6',
-            titulo: 'Procesos textiles',
-            hash: 't_3_6',
-          },
         ],
       },
     ],
     subMenu: [
-      {
-        nombreRuta: 'actividad',
-        icono: 'far fa-question-circle',
-        titulo: 'Actividad didáctica',
-        desarrolloContenidos: true,
-      },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
@@ -266,134 +107,101 @@ export default {
   referencias: [
     {
       referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA: ',
+        'Amadeus s.a.s. (2016). <i>Amadeus Altéa Reservation Desktop Web – User Guide. Edition 3.3.</i> Amadeus Learning Services.',
+    },
+    {
+      referencia:
+        'Amadeus s.a.s. (2016). <i>Amadeus Ancillary Services – Guía de Referencia Rápida.</i> Amadeus Soluciones Tecnológicas SA.',
+    },
+    {
+      referencia: 'Avianca S.A. (2021). <i>Carry-on baggage.</i>',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://www.avianca.com/otr/en/before-your-trip/prepare-your-baggage/carry-on/',
+    },
+    {
+      referencia: 'COPA Airlines Incorporated. (2020). Carry-on baggage.',
+      link: 'https://www.copaair.com/en/web/ca/carry-on-luggage',
     },
     {
       referencia:
-        'Castellanos, O., Fumeque A., y Ramirez D. (2011). Análisis de tendencias: de la información hacia la innovación. Universidad Nacional de Colombia. Sistema de Bibliotecas SENA: ',
+        'LATAM Airlines Group. (2020). Travel Information – Baggage – Carry-on.',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000007947',
+        'https://www.latam.com/en_us/travel-information/baggage/carry-on-baggage/',
     },
     {
       referencia:
-        'Harpersbazaar. (2019). La ropa que eliges afecta a tu estado de ánimo y cerebro, según expertos. En arpersbazaar.mx.',
-    },
-    {
-      referencia:
-        'Lafuente M., Navarro J., y Navarro J. (2005). Ilustración de moda. Sistema de Bibliotecas SENA',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000025549',
-    },
-    {
-      referencia:
-        'Laver, J.,  Albizua, H., y Fortea, B. (1990). Breve historia del traje y la moda. Sistema de Bibliotecas SENA. ',
-    },
-    {
-      referencia:
-        'Navarro, J.,  y Lafuente, M. (2010). Ilustración de moda: dibujo plano. Sistema de Bibliotecas SENA:',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/q6j6k0/sena_aleph000023241',
-    },
-    {
-      referencia:
-        'Áciedad Americana de Ensayos y Materiales (ASTM). (2003). Textiles. Editorial Staff. [Book]. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000051551',
-    },
-    {
-      referencia:
-        'Szkutnicka, B., Cuenca, C., Koyama, A. (2010). El dibujo técnico de moda paso a paso. Sistema de Bibliotecas SENA.',
-      link:
-        'https://sena-primo.hosted.exlibrisgroup.com/permalink/f/1j5choe/sena_aleph000057728',
+        'U.S. Department of Homeland Security. (2007). <i>DHS Pre-departure Aircraft Operator Compliance Requirements Reference Guide.</i> U.S. Customs and Border Protection Office of Information and Technology.',
     },
   ],
   glosario: [
     {
-      termino: 'MoodBoard',
-      significado:
-        'su traducción literal es mapa de inspiración y aunque su uso está muy asociado al ámbito de la decoración de interiores o la moda, <br/>realmente se utiliza en entornos creativos muy diversos, desde el diseño de productos o servicios al audiovisual o el diseño gráfico.',
+      termino: 'Airport special services',
+      significado: 'Servicios Especiales en el Aeropuerto.',
     },
     {
-      termino: 'Suftware',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+      termino: 'Associated emd (emd-a)',
+      significado: 'EMD Asociado.',
     },
     {
-      termino: 'Pirognóstico',
-      significado:
-        'ensayo sin componentes reactivos, quiere decir, reconocimiento de los componentes, mediante la aplicación adecuada del fuego.',
+      termino: 'Boarding announcements',
+      significado: 'Anuncios de Abordaje/Embarque.',
     },
     {
-      termino: 'Áectores',
-      significado:
-        'en software de diseño, son líneas que se crean y permiten, expandir, modificar, al tamaño que sea sin pixelarse porque se unen por coordenadas matemáticas del programa y no se generan por números fijos.',
+      termino: 'Boarding process',
+      significado: 'Proceso de Abordaje/Embarque.',
     },
     {
-      termino: 'Moda',
-      significado:
-        'hábito repetitivo que identifica a una persona, una especie de presión social, que debe consumir, utilizar o hacer.',
+      termino: 'Boarding process forms and reports',
+      significado: 'Formularios e Informes del Proceso de Abordaje/Embarque.',
     },
     {
-      termino: 'Diseño',
-      significado:
-        'actividad creativa orientada a soluciones idóneas a ciertas problemáticas.',
+      termino: 'Carry-on baggage',
+      significado: 'Equipaje de mano (también, Hand Luggage o Cabin Baggage).',
     },
     {
-      termino: 'Tipología',
-      significado:
-        'estudio de los tipos o modelos que se usan para clasificar en diversas ciencias o disciplinas.',
+      termino: 'Passenger security interview',
+      significado: 'Entrevista de Seguridad del Pasajero.',
     },
     {
-      termino: 'Bidimensional',
-      significado:
-        'que se representa según su altura y su anchura y no su profundidad.',
+      termino: 'Standalone emd (emd-s)',
+      significado: 'EMD Separado.',
     },
     {
-      termino: 'Interés Inferior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interseccionalidad',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
-    },
-    {
-      termino: 'Interés superior del menor',
-      significado:
-        'término informático que hace referencia a un programa o conjunto de programas de cómputo, así como datos, procedimientos y pautas que permiten realizar distintas tareas en un sistema informático.',
+      termino: 'Travel documents',
+      significado: 'Documentos de Viaje.',
     },
   ],
   complementario: [
     {
       texto:
-        'BIC., (2015). “2 minutos para entender el desarrollo sostenible – Spanish”',
-      tipo: 'Video',
-      link: 'https://www.google.com/',
+        'Management process in counter // <i>[Proceso de manejo en counter]</i>: Pearson. (s.f.). <i>Inglés para Turismo Internacional</i>.',
+      tipo: 'Página web',
+      link:
+        'https://www.pearson.com/english/catalogue/business-english/english-international-tourism.html',
     },
     {
       texto:
-        'Arnold, M. y Osorio, F., (1998). Introducción a los conceptos básicos de la Teoría general de sistemas. Cinta moebio 3: 40-49',
-      tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
+        'Management process in counter // <i>[Proceso de manejo en counter]</i>: Amadeus Get On Board. (2012) Amadeus Altea Customer Management Solution - Parts 1. [video]. Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=Sap7uNPgNdk',
     },
     {
       texto:
-        'Balboa, C. y Dominguez, M. (2014). Economía circular como marco para el ecodiseño: el modelo ECO-3. Informador Técnico (Colombia) Volumen 78, No. 1. Pág. 52-90.',
-      tipo: 'Artículo científico',
-      link: 'https://www.google.com/',
-    },
-    {
-      texto: 'Economía de Rosquilla. (s.f.).',
-      tipo: 'Página web',
-      descarga: '/downloads/prueba.pdf',
-    },
-    {
-      texto: '2 minutos para entender el desarrollo sostenible – Spanish',
+        'Management process in counter // <i>[Proceso de manejo en counter]</i>: Amadeus Get On Board. (2012) Amadeus Altea Customer Management Solution - Parts 2. [video]. Youtube.',
       tipo: 'Video',
-      link: 'https://www.google.com/',
+      link: 'https://www.youtube.com/watch?v=J_-F1tmDCxs&t=2s',
+    },
+    {
+      texto:
+        'Management process in counter // <i>[Proceso de manejo en counter]</i>: Amadeus Get On Board. (2012) <i>Amadeus Altea Customer Management Solution - Parts 3.</i> [video]. Youtube.',
+      tipo: 'Página web',
+      link: 'https://www.youtube.com/watch?v=i1nh9lEFaLY',
+    },
+    {
+      texto:
+        'Management process in counter // <i>[Proceso de manejo en counter]</i>: Amadeus IT Group. (2015). <i>Altéa Compensation Management.</i> [video]. Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=XPjAy-9i1Kk',
     },
   ],
   creditos: {
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -6,20 +6,150 @@
     .titulo-principal
       .titulo-principal__numero
         span  2
-      h1 Management process in counter // Proceso de manejo en counter
+      h1 Passenger boarding process // <i>Proceso de embarque del pasajero</i>
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        figure
+          img(src='@/assets/curso/t2-i1.svg', alt='imagen decorativa alusiva a pasajeros haciendo fila para abordar un avion')
+    
+    .cajon.color-secundario.p-5.mb-5
+      p The passenger boarding process begins at the check-in counter, when travelers are given all the detailed information on their boarding pass related to the departure lounge. This is the same information that is displayed on the screens inside the airport facilities. The departure lounge is the waiting room closest to the departure gate of your flight. It is there where the airline staff will be assisting the passengers in boarding the plane.
 
-      #t_2_1.titulo-segundo
-        h2 2.1 Making of boarding announcements at the boarding gate área // Manejo de anuncios en sala de abordaje
+    p.mb-5.text-it  El proceso de embarque de pasajeros inicia cuando en el mostrador de registro se les entrega a los viajeros toda la información detallada en su tarjeta de embarque de la sala de abordaje que es la misma en las pantallas informativas que hay dentro de las instalaciones del aeropuerto. La sala de embarque es la sala de espera más próxima a la puerta de su vuelo, es allí donde el personal de la aerolínea dará el manejo a los pasajeros para poder acceder al avión.
 
+    #t_2_1.titulo-segundo
+      h2 2.1 Making of boarding announcements at the boarding gate área // <i>Manejo de anuncios en sala de abordaje</i>
+    .row.mb-5
+      .col-lg-3
+        figure
+          img(src='@/assets/curso/t2-i2.svg', alt='Icono decorativo')
+      .col-lg-9
+        .cajon.color-secundario.p-5.mb-5
+          p One of the steps in the boarding process are boarding announcements that provide passengers with information about the flight and its possible changes. It is important to make these announcements periodically to guide travelers.
+        p Uno de los pasos en el proceso de abordaje son los anuncios de abordaje que brindan información a los pasajeros acerca del vuelo y sus posibles novedades. Es importante hacer estos anuncios periódicamente para orientar a los viajeros.
 
-      #t_2_2.titulo-segundo
-        h2 2.2  Completion of reports and forms for the boarding process // Diligenciamiento de informes y formatos para el proceso de abordaje
+    h3.titulo-tercero A. Guidelines for making boarding announcements at the boarding gate area // Parámetros para los anuncios en sala de abordaje.
+    .row.justify-content-center.mb-5
+      .col-lg-6
+        figure
+          img(src='@/assets/curso/t2-i3.svg', alt='imagen decorativa')
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        .card.bg-soft-gray
+          .card-title.color-acento-botones
+            .h4 Structure of a boarding announcement
+          .card-body.px-5
+             ol.lista-ol
+              li 
+                span.text-normal-bold 1. 
+                p.text-bold.text-it  Greeting // 
+                  span.text-normal (Saludar)
+              li 
+                span.text-normal-bold 2. 
+                p.text-bold.text-it  Tell the name of the Airline // 
+                  span.text-normal  (Decir el nombre de la aerolínea)                    
+              li 
+                span.text-normal-bold 3. 
+                p.text-bold.text-it  State clearly the Flight Number // 
+                  span.text-normal (Decir claramente el número del vuelo)
+              li 
+                span.text-normal-bold 4. 
+                p.text-bold.text-it  State the Route // 
+                  span.text-normal (Decir la ruta)
+              li 
+                span.text-normal-bold 5. 
+                p.text-bold.text-it  Tell the Boarding Gate Number // 
+                  span.text-normal (Decir el Número de la Sala de Abordaje)
+              li 
+                span.text-normal-bold 6. 
+                p.text-bold.text-it  Inform of any changes that may arise // 
+                  span.text-normal (Informar alguna novedad que haya surgido)                 
+    
+    h3.titulo-tercero B. Sample Boarding Announcements. With the help of your tutor, explore the different elements in the following announcements and practice reading them out loud. // <i>Ejemplos de anuncios de embarque. Con la ayuda del instructor, explorar los diferentes elementos de los siguientes anuncios y practicar su lectura en voz alta.</i>
+    .row.justify-content-center.mb-5
+      .col-lg-3
+        figure
+          img.nW(src='@/assets/curso/t2-i4.svg', alt='una auxiliar de vuelo de pie')
+      .col-lg-7
+        AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta tarjeta--gris")
+          .row(titulo="Pre-boarding Announcement")
+            .col-md-10.mb-4.mb-md-0
+              p Good afternoon, ladies and gentlemen! Your Sena Airways flight SA433 to Cali is now ready for boarding. Please, proceed to Gate B3. We kindly ask Business Class passengers and passengers with small children to proceed to the airplane at their convenience without queuing. Passengers requiring special assistance can also begin boarding now. Please have your boarding pass and ID ready. Regular boarding will start in approximately twelve minutes. Thank you.      
 
+          .row(titulo="Announcing a Flight Delay")
+            .col-md-10.mb-4.mb-md-0
+              p Good morning. This announcement is for passengers travelling on the 8:25 flight SA387 to Baltimore with stops in Panama City and Miami. The boarding Gate has been changed to C2. The flight is delayed by one hour and forty minutes due to bad weather. The flight is also slightly overbooked, so we are offering a complimentary travel voucher worth $300 to a few passengers willing to take a later flight. Our new departure time is 10:05. Thank you for your patience.    
 
+          .row(titulo="Final Boarding Call Announcement")
+            .col-md-10.mb-4.mb-md-0
+              p This is the final boarding call for passengers travelling to Baltimore on Sena Airways flight SA387 due to leave at 10:05. Will any remaining passengers, please, proceed to Gate C2 immediately, where the flight is now closing? I repeat. This is the final boarding call for passengers travelling to Baltimore on Sena Airways flight SA387. Thank you.    
+    
+    
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .card.bg-soft-blue
+          .row
+            .col-lg-9
+              .p-4
+                ul.lista-ul.text-it
+                  li.text-bold Ready for boarding  
+                    span.text-normal // Listo para abordar
+                  li.text-bold Proceed to Gate B3  
+                    span.text-normal // Proceda a la Puerta B3
+                  li.text-bold  Without queuing   
+                    span.text-normal // Sin hacer fila
+                  li.text-bold  With stops in… 
+                    span.text-normal // Con escalas en…
+                  li.text-bold  Due to bad weather  
+                    span.text-normal // Debido al mal tiempo
+                  li.text-bold  The flight is also slightly overbooked   
+                    span.text-normal // El vuelo también está ligeramente sobrevendido
+                  li.text-bold A complimentary travel voucher worth $300  
+                    span.text-normal // Un voucher de viaje de cortesía por valor de $250
+                  li.text-bold  Due to leave at…   
+                    span.text-normal // Previsto para salir a las…
+                  li.text-bold  Remaining passengers   
+                    span.text-normal // Los pasajeros que hacen falta
+                  li.text-bold  The flight is now closing   
+                    span.text-normal  // El vuelo ahora se está cerrando                    
 
+                    
+            .col-lg-3.align-items-bottom
+              figure
+                img(src='@/assets/curso/t2-i5.svg', alt='una mujer pensando')
 
+    #t_2_2.titulo-segundo
+      h2 2.2  Completion of reports and forms for the boarding process // <i>Diligenciamiento de informes y formatos para el proceso de abordaje</i>
 
+    .row.justify-content-center.mb-5
+      .col-lg-7
+        .cajon.color-secundario.p-5.mb-5
+          p The passenger boarding process begins at the check-in counter, when travelers are given all the detailed information on their boarding pass related to the departure lounge. This is the same information that is displayed on the screens inside the airport facilities. The departure lounge is the waiting room closest to the departure gate of your flight. It is there where the airline staff will be assisting the passengers in boarding the plane.
+        p.text-it El cierre del vuelo al finalizar el abordaje es de suma importancia ya que en este está el detallado de novedades del vuelo, es decir, información acerca de la tripulación de cabina de mando y de cabina de pasajeros que opera el vuelo, cuántos pasajeros viajan en las diferentes cabinas (primera clase, ejecutiva y económica), infantes en el vuelo, cuántos adultos, el peso del vuelo, si en este vuelo están abordados empleados de la misma aerolínea, los servicios especiales solicitados en el vuelo, etc.
+      .col-lg-5
+        figure
+          img(src='@/assets/curso/t2-i6.png', alt='sillas de sala de espera en un aeropuerto')
 
+    .row.justify-content-center.mb-3
+      .col-lg-10
+        .cajon.color-secundario.p-5.mb-5
+          .bloque-texto-b.color-secundario.p-4
+            .bloque-texto-b__texto
+              i.fas.fa-quote-left
+              p.mb-0 <b>The Flight Close-Out</b> message indicates the actual date and time of departure of a flight, the total passengers onboard, and identifies travelers, using their unique identifier, who boarded the aircraft. Two types of Flight Close-Out messages are available. One option indicates the unique identifiers of passengers who were boarded. A second option indicates the unique identifiers of passengers who were not boarded.<br>
+              .bloque-texto-d__autor
+                p.text-bold According to DHS Pre-departure Aircraft Operator Compliance Requirements Reference Guide (2007, p.24)
+              i.fas.fa-quote-right
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .p-5
+          .bloque-texto-b.color-secundario.p-4
+            .bloque-texto-b__texto
+              i.fas.fa-quote-left
+              p.mb-0 <b>El mensaje de cierre de vuelo </b> indica la fecha y hora reales de salida de un vuelo, el total de pasajeros a bordo e identifica a los viajeros, utilizando su identificador único, que abordaron la aeronave. Hay dos tipos de mensajes de cierre de vuelo disponibles. Una opción indica los identificadores únicos de los pasajeros que fueron abordados. Una segunda opción indica los identificadores únicos de los pasajeros que no fueron abordados.<br>
+              .bloque-texto-d__autor
+                p.text-bold Según la DHS Pre-departure Aircraft Operator Compliance Requirements Reference Guide (2007, p.24)
+              i.fas.fa-quote-right
 
 
 
